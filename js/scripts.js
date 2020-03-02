a4b952b320857a68f6716b99f3719135d0a6539a
J-grigri <jurgis.grigorjevs@gmail.com>
2020-03-02 23:12:43 +0700

    Multiple currencies and improved code

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,51 @@
+
 const vndUsd = 23208;
-
 const usdVnd = 1 / vndUsd;
+const vndEur = 25836;
+const eurVnd = 1 / vndEur;
+const vndKrw = 19.5;
+const krwVnd = 1 / vndKrw;
+const vndIdr = 1.63
+const IdrVnd = 1 / vndIdr;
 
+const start = prompt("select currency on hand");
+const end = prompt("select currency to exchange to");
+const currency = start.toLowerCase() + "-" + end.toLowerCase();
 
-function vndToUsd(amountVnd) {
-    console.log("VND to USD", (amountVnd / vndUsd).toFixed(2))
-}
-function usdToVnd(amountUsd) {
-    console.log("USD to VND", (amountUsd / usdVnd).toFixed(2))
-}
-let currency (prompt"select currency");
-if (currency === "vnd") {
-    let amountVnd = prompt("Amount of VND?");
-    if (isNaN(amountVnd)) {
-        alert("insert valid number")
+let amount = prompt("amount");
+if (amount > 0) {
+    switch (currency) {
+        case "vnd-usd":
+            console.log((amount / vndUsd).toLocaleString('en-US', { style: 'currency', currency: 'USD', }))
+            break;
+        case "usd-vnd":
+            console.log((amount / usdVnd).toLocaleString('vn-VN', { style: 'currency', currency: 'VND', }))
+            break;
+        case "vnd-eur":
+            console.log((amount / vndEur).toLocaleString('de-DE', { style: 'currency', currency: 'EUR', }))
+            break;
+        case "eur-vnd":
+            console.log((amount / eurVnd).toLocaleString('vn-VN', { style: 'currency', currency: 'VND', }))
+            break;
+        case "vnd-krw":
+            console.log((amount / vndKrw).toLocaleString('kr-KR', { style: 'currency', currency: 'KRW', }))
+            break;
+        case "krw-vnd":
+            console.log((amount / krwVnd).toLocaleString('vn-VN', { style: 'currency', currency: 'VND', }))
+            break;
+        case "vnd-idr":
+            console.log((amount / vndIdr).toLocaleString('id-ID', { style: 'currency', currency: 'IDR', }))
+            break;
+        case "idr-vnd":
+            console.log((amount / IdrVnd).toLocaleString('vn-VN', { style: 'currency', currency: 'VND', }))
+            break;
+
+        default:
+            console.log("Insert positive number")
     }
-    else if (amountVnd < 0) {
-        alert("insert positive number");
-    }
-    else {
-        vndToUsd(amountVnd)
-    }
+} else {
+    console.log("enter a number")
 }
-
-else if (currency === "usd") {
-    let amountUsd = prompt("Amount of USD?");
-    if (isNaN(amountUsd)) {
-        if (amounUsd > 0)
-            alert("Only positive numbers are accepted");
-        else alert ("Please input a number");
-    }
-
-    else {
-        usdToVnd(amountUsd)
-    }
-}
-
-else {
-    alert("Please choose one of the supported currencies: VND | USD");
-    prompt("Choose one of the following input currencies: VND | USD")
-}
-
-
-
-// const start = prompt("select currency on hand")
-// const end = prompt("select currency to exchange to")
-// const currency = start.toLowerCase()+ "-" + end.toLowerCase()
-// let amount = prompt ("amount")
-// if (amount>0){
-//     switch (currency) {
-//         case "vnd-usd" :
-//             console.log("VND to USD", (amount / vndUsd).toFixed(2))
-//             break;
-//         case "usd-vnd":
-//             console.log("USD to VND", (amount / usdVnd).toFixed(2))
-//             break;
-//         default:
-//             console.log("enter an pr...")
-//     }
-// } else {
-//     console.log("enter a number")
-// }
 
 
 
